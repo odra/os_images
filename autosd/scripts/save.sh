5bf2d6b218404bf8a41802f21b61726939ac5a3f
Leonardo Rossetti <lrossett@redhat.com>
2026-02-25 07:24:22 -0300

    adding rpi4 target
    Signed-off-by: Leonardo Rossetti <lrossett@redhat.com>

--- autosd/scripts/save.sh
+++ autosd/scripts/save.sh
@@ -16,8 +16,14 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 source ${SCRIPT_DIR}/vars.sh
 source ${SCRIPT_DIR}/aib.sh
 
-aib::oci_to_disk_image ${AIB_OCI_IMAGE} ${AIB_BUILD_DIR}/outputs/score-autosd-$(arch)-latest.qcow2
-aib::oci_export ${AIB_BUILD_DIR} ${AIB_OCI_IMAGE} score-autosd-$(arch)-latest-qemu.tar
+if [ "${AIB_TARGET}" = "qemu" ]; then
+  DISK_IMG_EXT="qcow2"
+else
+  DISK_IMG_EXT="img"
+fi
+
+aib::oci_to_disk_image ${AIB_OCI_IMAGE} ${AIB_BUILD_DIR}/outputs/score-autosd-$(arch)-latest.${DISK_IMG_EXT}
+aib::oci_export ${AIB_BUILD_DIR} ${AIB_OCI_IMAGE} score-autosd-$(arch)-latest-${AIB_TARGET}.tar
 
 sudo chown -R $(id -u):$(id -u) ${AIB_BUILD_DIR}
 
